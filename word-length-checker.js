26badc7c83d4499c0ef56ebf5f71635e897187cd
STAYCUTE <34066247+staycuteangelanddevil@users.noreply.github.com>
2019-08-14 20:32:47 +0900

    Create word-length-checker.js

---
+++ word-length-checker.js
@@ -0,0 +1,270 @@
+var insertword = 'Hello World' //이곳에 글자수를 체크할 글을 써주세요 영어만 가능합니다.
+
+
+var word = [] //객체를 저장할 공간
+var Blank = 0 //공백은 이곳에
+for (var insert = 0; insertword.length > insert; insert++) {
+    if (insertword.charAt(insert) == " ") {
+        Blank++ //공백일시 Blank에 +1
+    } else {
+        word.push(insertword.charAt(insert)) //공백이 아닐시 word에 저장
+    }
+}
+
+var one = 0
+var two = 0
+var three = 0
+var four = 0
+var five = 0
+var six = 0
+var seven = 0
+var eight = 0
+var nine = 0
+var zero = 0
+
+var a = 0
+var b = 0
+var c = 0
+var d = 0
+var e = 0
+var f = 0
+var g = 0
+var h = 0
+var i = 0
+var j = 0
+var k = 0
+var m = 0
+var n = 0
+var l = 0
+var o = 0
+var p = 0
+var q = 0
+var r = 0
+var s = 0
+var t = 0
+var u = 0
+var v = 0
+var w = 0
+var x = 0
+var y = 0
+var z = 0
+
+var A = 0
+var B = 0
+var C = 0
+var D = 0
+var E = 0
+var F = 0
+var G = 0
+var H = 0
+var I = 0
+var J = 0
+var K = 0
+var M = 0
+var N = 0
+var L = 0
+var O = 0
+var P = 0
+var Q = 0
+var R = 0
+var S = 0
+var T = 0
+var U = 0
+var V = 0
+var W = 0
+var X = 0
+var Y = 0
+var Z = 0
+
+for (var forcount = 0; word.length > forcount; forcount++) {
+    if (word[forcount - 1] == 'A') {
+        A++
+        count++
+    } else if (word[forcount - 1] == 'B') {
+        B++
+        count++
+    } else if (word[forcount - 1] == 'C') {
+        C++
+        count++
+    } else if (word[forcount - 1] == 'D') {
+        D++
+        count++
+    } else if (word[forcount - 1] == 'E') {
+        E++
+        count++
+    } else if (word[forcount - 1] == 'F') {
+        F++
+        count++
+    } else if (word[forcount - 1] == 'G') {
+        G++
+        count++
+    } else if (word[forcount - 1] == 'H') {
+        H++
+        count++
+    } else if (word[forcount - 1] == 'I') {
+        I++
+        count++
+    } else if (word[forcount - 1] == 'J') {
+        J++
+        count++
+    } else if (word[forcount - 1] == 'K') {
+        K++
+        count++
+    } else if (word[forcount - 1] == 'L') {
+        L++
+        count++
+    } else if (word[forcount - 1] == 'M') {
+        M++
+        count++
+    } else if (word[forcount - 1] == 'N') {
+        N++
+        count++
+    } else if (word[forcount - 1] == 'O') {
+        O++
+        count++
+    } else if (word[forcount - 1] == 'P') {
+        P++
+        count++
+    } else if (word[forcount - 1] == 'Q') {
+        Q++
+        count++
+    } else if (word[forcount - 1] == 'R') {
+        R++
+        count++
+    } else if (word[forcount - 1] == 'S') {
+        S++
+        count++
+    } else if (word[forcount - 1] == 'T') {
+        T++
+        count++
+    } else if (word[forcount - 1] == 'U') {
+        U++
+        count++
+    } else if (word[forcount - 1] == 'V') {
+        V++
+        count++
+    } else if (word[forcount - 1] == 'W') {
+        W++
+        count++
+    } else if (word[forcount - 1] == 'X') {
+        X++
+        count++
+    } else if (word[forcount - 1] == 'Y') {
+        Y++
+        count++
+    } else if (word[forcount - 1] == 'Z') {
+        Z++
+        count++
+    } else if (word[forcount - 1] == 'a') {
+        a++
+        count++
+    } else if (word[forcount - 1] == 'b') {
+        b++
+        count++
+    } else if (word[forcount - 1] == 'c') {
+        c++
+        count++
+    } else if (word[forcount - 1] == 'd') {
+        d++
+        count++
+    } else if (word[forcount - 1] == 'e') {
+        e++
+        count++
+    } else if (word[forcount - 1] == 'f') {
+        f++
+        count++
+    } else if (word[forcount - 1] == 'g') {
+        g++
+        count++
+    } else if (word[forcount - 1] == 'h') {
+        h++
+        count++
+    } else if (word[forcount - 1] == 'i') {
+        i++
+        count++
+    } else if (word[forcount - 1] == 'j') {
+        j++
+        count++
+    } else if (word[forcount - 1] == 'k') {
+        k++
+        count++
+    } else if (word[forcount - 1] == 'l') {
+        l++
+        count++
+    } else if (word[forcount - 1] == 'm') {
+        m++
+        count++
+    } else if (word[forcount - 1] == 'n') {
+        n++
+        count++
+    } else if (word[forcount - 1] == 'o') {
+        o++
+        count++
+    } else if (word[forcount - 1] == 'p') {
+        p++
+        count++
+    } else if (word[forcount - 1] == 'q') {
+        q++
+        count++
+    } else if (word[forcount - 1] == 'r') {
+        r++
+        count++
+    } else if (word[forcount - 1] == 's') {
+        s++
+        count++
+    } else if (word[forcount - 1] == 't') {
+        t++
+        count++
+    } else if (word[forcount - 1] == 'u') {
+        u++
+        count++
+    } else if (word[forcount - 1] == 'v') {
+        v++
+        count++
+    } else if (word[forcount - 1] == 'w') {
+        w++
+        count++
+    } else if (word[forcount - 1] == 'x') {
+        x++
+        count++
+    } else if (word[forcount - 1] == 'y') {
+        y++
+        count++
+    } else if (word[forcount - 1] == 'z') {
+        z++
+        count++
+    } else if (word[forcount - 1] == '1') {
+        one++
+        count++
+    } else if (word[forcount - 1] == '2') {
+        two++
+        count++
+    } else if (word[forcount - 1] == '3') {
+        three++
+        count++
+    } else if (word[forcount - 1] == '4') {
+        four++
+        count++
+    } else if (word[forcount - 1] == '5') {
+        five++
+        count++
+    } else if (word[forcount - 1] == '6') {
+        six++
+        count++
+    } else if (word[forcount - 1] == '7') {
+        seven++
+        count++
+    } else if (word[forcount - 1] == '8') {
+        eight++
+        count++
+    } else if (word[forcount - 1] == '9') {
+        nine++
+        count++
+    } else if (word[forcount - 1] == '0') {
+        zero++
+        count++
+    }
+
+
+}
+console.log(`Length : ${insertword.length}\nBlank : ${Blank}\nA : ${A}\nB : ${B}\nC : ${C}\nD : ${D}\nE : ${E}\nF : ${F}\nG : ${G}\nH : ${H}\nI : ${I}\nJ : ${J}\nK : ${K}\nL : ${L}\nM : ${M}\nN : ${N}\nO : ${O}\nP : ${P}\nQ : ${Q}\nR : ${R}\nS : ${S}\nT : ${T}\nU : ${U}\nV : ${V}\nW : ${W}\nX : ${X}\nY : ${Y}\nZ : ${Z}\na : ${a}\nb : ${b}\nc : ${c}\nd : ${d}\ne : ${e}\nf : ${f}\ng : ${g}\nh : ${h}\ni : ${i}\nj : ${j}\nk : ${k}\nl : ${l}\nm : ${m}\nn : ${n}\no : ${o}\np : ${p}\nq : ${q}\nr : ${r}\ns : ${s}\nt : ${t}\nu : ${u}\nv : ${v}\nw : ${w}\nx : ${x}\ny : ${y}\nz : ${z}\n1 : ${one}\n2 : ${two}\n3 : ${three}\n4 : ${four}\n5 : ${five}\n6 : ${six}\n7 : ${seven}\n8 : ${eight}\n9 : ${nine}\n0 : ${zero}`)
