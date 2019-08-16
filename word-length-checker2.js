9e899a1f506bfc4372aba1072736091cc9e366e8
STAYCUTE <34066247+staycuteangelanddevil@users.noreply.github.com>
2019-08-16 13:25:26 +0900

    Create word-length-checker2.js

---
+++ word-length-checker2.js
@@ -0,0 +1,35 @@
+const obj = {}
+var ary = new Array();
+const txt = 'Hello World / 안녕 세상 / こんにちは世界 / 你好，世界'
+const arr = txt.split('')
+arr.forEach(i => {
+    if (!obj[i]) {
+        if (i == ' ') {
+            if (!obj['공백']) {
+                obj['공백'] = 0
+            }
+            obj['공백']++
+                return;
+        } else {
+            obj[i] = 0
+        }
+    }
+    obj[i]++
+})
+
+var sortable = [];
+var sortable2 = [];
+for (var word in obj)
+    sortable.push([word, obj[word]])
+for (var word in obj)
+    sortable2.push([word, obj[word]])
+sortable.sort(function(a, b) {
+    return b[1] - a[1]
+})
+sortable2.sort(function(a, b) {
+    return a < b ? -1 : a > b ? 1 : 0;
+});
+
+console.log(sortable)
+console.log(sortable2)
+console.log(`${txt.length}`)
